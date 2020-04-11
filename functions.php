e826208a0846920f489d0f7f230c06a8184a7b25
Christoph Buehler <christoph@cbuehler.li>
2020-04-12 00:53:42 +0200

    asd

--- functions.php
+++ functions.php
@@ -133,11 +133,6 @@ function cbuehlerli_scripts() {
 add_action( 'wp_enqueue_scripts', 'cbuehlerli_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
