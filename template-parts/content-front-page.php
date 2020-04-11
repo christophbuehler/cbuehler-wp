29be51f7aa4996cb6b0db8041a97f126a2a4a6e7
Christoph Buehler <christoph@cbuehler.li>
2020-04-12 01:02:18 +0200

    asd

--- template-parts/content-front-page.php
+++ template-parts/content-front-page.php
@@ -8,6 +8,25 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="block">
-		Lorem Ipsum Dolor sit Amet
+		<?php the_title('<h1>', '</h1>'); ?>
+		<?php
+		the_content(sprintf(
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'cbuehlerli'),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
+		));
+
+		wp_link_pages(array(
+			'before' => '<div class="page-links">' . esc_html__('Pages:', 'cbuehlerli'),
+			'after'  => '</div>',
+		));
+		?>
 	</div>
 </article>
