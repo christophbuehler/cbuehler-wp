357a05e51fe751011ae038e7b0086ab878275920
Christoph Buehler <christoph@cbuehler.li>
2020-04-12 17:30:48 +0200

    removed custom-background

--- functions.php
+++ functions.php
@@ -62,10 +62,10 @@ if (!function_exists('cbuehlerli_setup')) :
 		));
 
 		// Set up the WordPress core custom background feature.
-		add_theme_support('custom-background', apply_filters('cbuehlerli_custom_background_args', array(
-			'default-color' => '000000',
-			'default-image' => '',
-		)));
+		// add_theme_support('custom-background', apply_filters('cbuehlerli_custom_background_args', array(
+		// 	'default-color' => '000000',
+		// 	'default-image' => '',
+		// )));
 
 		// Add theme support for selective refresh for widgets.
 		add_theme_support('customize-selective-refresh-widgets');
