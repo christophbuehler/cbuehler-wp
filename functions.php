bdd22e97967d2a8b32a5f4f4db5f39a33ea68f73
Christoph Buehler <christoph@cbuehler.li>
2020-04-22 06:29:49 +0200

    removed non required includes

--- functions.php
+++ functions.php
@@ -128,10 +128,6 @@ function cbuehlerli_scripts()
 {
 	wp_enqueue_style('cbuehlerli-style', get_stylesheet_uri());
 
-	wp_enqueue_script('cbuehlerli-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true);
-
-	wp_enqueue_script('cbuehlerli-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
-
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}
