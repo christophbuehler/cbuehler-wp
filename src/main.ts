d19f3fdbdcdb964aac4072171f15f64b6d85c89b
Christoph Buehler <christoph@cbuehler.li>
2020-09-30 18:40:58 +0200

    new animation

--- src/main.ts
+++ src/main.ts
@@ -22,104 +22,132 @@ function handleScroll() {
 function bg() {
   const canvas = document.querySelector('.bg') as HTMLCanvasElement;
   const ctx = canvas.getContext('2d');
+  const sticks = Sticks(canvas, ctx);
 
-  setInterval(update, 40);
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+
+  setInterval(() => sticks.update(), 20);
+  paint();
+  function paint() {
+    sticks.paint();
+    requestAnimationFrame(() => paint());
+  }
+}
+
+function Sticks(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
   let time = 0;
-  let points: Point[][];
-  let cPos: V2;
-  let target: V2;
-  const s = 20;
+
+  return {
+    update,
+    paint,
+  };
 
   function update() {
-    ++time;
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
+    time++;
+  }
+
+  function paint() {
     const dim = new V2(canvas.width, canvas.height);
-    const center = dim.div(V2.xy(2));
-    cPos = cPos ?? center;
-    points = points ?? getPointGrid(s, dim);
     ctx.clearRect(0, 0, dim.x, dim.y);
-
-    points.forEach(a => a.forEach(p => p.update(cPos)));
-
-    if (!target || target.sub(cPos).hyp() < 4) {
-      target = dim.dot(new V2(Math.random(), Math.random()));
+    const s = 8;
+    const p = 2;
+    ctx.fillStyle = 'white';
+    for (let y = 0; y < dim.y; y += s + p) {
+      ctx.fillStyle = `rgba(255,255,255,${(y % 100) / 180})`;
+      ctx.fillRect(80, y, 180 * Math.sin((time + y) * .01) + 20, s);
     }
-    cPos = target.sub(cPos).len(4).add(cPos);
-    points.forEach(col => {
-      col.forEach((point) => {
-        if (point.color === 'transparent') return;
-        point.draw(ctx, s);
+  }
+}
+
+function Net(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+  const dim = new V2(canvas.width, canvas.height);
+
+  let lines: V2[][] = [
+    getLine(dim),
+    getLine(dim),
+    getLine(dim),
+    getLine(dim),
+    getLine(dim),
+    getLine(dim),
+    getLine(dim),
+    getLine(dim),
+  ];
+
+  return {
+    update,
+    paint,
+  };
+
+  function update() {
+    const speed = .01;
+    lines.forEach((line, i) => {
+      const otherLine = lines[lines.length - 1 - i];
+      line.find((pos, h) => {
+        const target = otherLine[h];
+        const dir = target.sub(pos);
+
+        if (dir.hyp() <= speed) {
+          // lines[i] = getLine(dim);
+          return true;
+        }
+
+        const newPos = pos.add(dir.len(speed));
+        line[h] = newPos;
       });
     });
   }
 
-  function getPointGrid(s: number, dim: V2) {
-    const rows = Array.from(Array(~~(dim.y / s) + 1)).map((_, i) => i);
-    const cols = Array.from(Array(~~(dim.x / s) + 1)).map((_, i) => i);
-    return rows
-      .map((r, i) => cols
-        .filter(() => Math.random() > 0)
-        .map(c => new Point(new V2((c  + (i % 2) * .5) * s, (r) * s))));
+  function paint() {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    const dim = new V2(canvas.width, canvas.height);
+    ctx.clearRect(0, 0, dim.x, dim.y);
+
+    lines.forEach((line, i) => {
+      ctx.beginPath();
+      line.forEach((pos, h) => ctx[h === 0 ? 'moveTo' : 'lineTo'](pos.x, pos.y));
+
+      ctx.strokeStyle = '#aaa';
+      ctx.lineWidth = 2;
+      ctx.stroke();
+      ctx.closePath();
+
+      ctx.beginPath();
+
+      line.forEach(pos => {
+        lines.slice(i + 1, lines.length).flat()
+          .filter(lineTo => lineTo.sub(pos).hyp() < 40)
+          .forEach(lineTo => {
+            ctx.moveTo(pos.x, pos.y);
+            ctx.lineTo(lineTo.x, lineTo.y);
+          });
+      });
+      ctx.closePath();
+
+      ctx.strokeStyle = '#eee';
+      ctx.lineWidth = 1;
+      ctx.stroke();
+    });
   }
 }
 
-class Point {
-  private initialPos: V2;
-
-  color: string;
-
-  constructor(
-    public pos: V2,
-  ) {
-    this.initialPos = pos;
+function getLine(dim: V2) {
+  const len = 102;
+  const d = 120;
+  const line = [dim.dot(new V2(
+    .5,
+    .5,
+  ))];
+  let last: V2;
+  for (let i=0; i<len; i++) {
+    last = line[line.length - 1];
+    line.push(new V2(
+      ~~(Math.random() * d) - d / 2,
+      ~~(Math.random() * d) - d / 2,
+    ).add(last));
   }
-
-  draw(ctx: CanvasRenderingContext2D, s: number) {
-    ctx.beginPath();
-    ctx.save();
-    ctx.translate(this.pos.x, this.pos.y);
-
-    s = s - 4;
-
-    const hs = s;
-
-
-    ctx.moveTo(-s * .5, -s * .5);
-    ctx.lineTo(s * .5, s * .5);
-
-
-    // ctx.moveTo(-s * .5, hs * .5);
-    // ctx.lineTo(-s * .5, -hs * .5);
-    // ctx.lineTo(0, -hs * .5);
-    // ctx.lineTo(s * .5, -hs * .5);
-    // ctx.lineTo(s * .5, hs * .5);
-
-    ctx.closePath();
-    ctx.strokeStyle = this.color;
-    ctx.stroke();
-    ctx.restore();
-  }
-
-  update(mPos: V2) {
-    const len = 140;
-    const deltaPos = this.initialPos
-      .sub(mPos);
-
-    if (deltaPos.hyp() > len + 120) {
-      this.color = 'transparent';
-    } else {
-      const alpha = ((len - deltaPos.hyp()) / len);
-      this.color = `rgba(255,255,255,${Math.max(alpha, .2)})`;
-    }
-
-    if (deltaPos.hyp() > len) {
-      this.pos = this.initialPos;
-      return;
-    }
-
-    const height = 40;
-    this.pos = this.initialPos
-      .add(new V2(0, -((1 - deltaPos.hyp() / len)) * height));
-  }
+  return line;
 }
