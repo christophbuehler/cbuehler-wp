9d1ffd6389ccb4c46b6bba032e5efa792bf4ed3c
Christoph Buehler <christoph@cbuehler.li>
2020-10-01 10:31:07 +0200

    css cleanup

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
-    <?php wp_enqueue_script('script', get_template_directory_uri() . '/assets/cbuehler.min.js', [], '1.0.23'); ?>
+    <?php wp_enqueue_script('script', get_template_directory_uri() . '/assets/cbuehler.min.js', [], '1.0.24'); ?>
     <?php wp_head(); ?>
 </head>
 
@@ -29,7 +29,7 @@ $image = wp_get_attachment_image_src($custom_logo_id, 'full');
             </div>
         </header>
         <nav>
-            <div class="sub-title">Recent Articles</div>
+            <div class="sub-title">Recent</div>
 
             <?php
             $args = array(
